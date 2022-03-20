e8d0da9d8ad5f5f50e15c81bcb6b872b44c165d6
Rafael Fonseca <fonseca.wutang@gmail.com>
2022-03-20 16:41:12 +0000

    add first solution

---
+++ EscreverPorExtenso.py
@@ -0,0 +1,6 @@
+data = int(input("Digite um numero de 0 a 20: "))
+
+numeros = ("zero", "um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove", "vinte")
+
+escrito = numeros[data]
+print(escrito)
